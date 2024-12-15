59c01643daa01da8cf6c5a2fc5d4ba5a24d0fcee
emile <emilepicard323@gmail.com>
2024-12-15 20:36:27 +0100

    Maj Use UUID

--- app/Views/UserController.py
+++ app/Views/UserController.py
@@ -1,83 +1,126 @@
-# from django.http import JsonResponse
-# from django.contrib.auth.models import User
-import pdb
+import uuid
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password, check_password
 from django.http import HttpResponse, JsonResponse
+from app.Models.Group import Group
 from app.Models.User import User
 from app.Views.Controller import Controller
 
-
-class UserController(Controller):
-    
+class GroupController(Controller):
     def __init__(self):
         super().__init__()
-    
+
     def get(self, request):
-        # you are suppose to get users with the ORM here
-        getid = request.GET.get('id')
-        # this line create a new user, its just to show you the serializer
-        if getid != None :
-            test_user = User.objects.get(id=getid)
-        else :
-            test_user = User.objects.all()
-        # pdb.set_trace()
-        return super().serialize(test_user, "user") #this route is a "user" type route, this mean only uuid will be returned, if you want to also return created_at, use "admin"
-            
+        get_uuid = request.GET.get('uuid')
+        if get_uuid:
+            group = Group.objects.get(uuid=get_uuid)
+        else:
+            group = Group.objects.all()
+        return super().serialize(group, "user")
+
+    def post(self, request):
+        name = request.POST.get('name')
+        new_group = Group(name=name)
+        new_group.save()
+        messages.success(request, "Formulaire soumis avec succès !")
+        return super().serialize(new_group, "user")
+
+    def set_user_to_group(self, request):
+        user_uuid = request.GET.get('user_uuid')
+        group_uuid = request.GET.get('group_uuid')
+        try:
+            user = User.objects.get(uuid=user_uuid)  # Recherche  UUID.
+            group = Group.objects.get(uuid=group_uuid)  # Recherche  UUID.
+            user.groups.add(group)
+            group.users.add(user)
+            user.save()
+            group.save()
+        except User.DoesNotExist:
+            return HttpResponse("Utilisateur non trouvé", status=404)
+        except Group.DoesNotExist:
+            return HttpResponse("Groupe non trouvé", status=404)
+        return HttpResponse("Utilisateur lié au groupe avec succès")
+
+    def update_group(self, request):
+        group_uuid = request.GET.get('uuid')  # Utilisation UUID
+        name = request.POST.get('name')
+        description = request.POST.get('description', None)
+        try:
+            group = Group.objects.get(uuid=group_uuid)
+            group.name = name
+            if description:
+                group.description = description
+            group.save()
+        except Group.DoesNotExist:
+            return HttpResponse("Groupe non trouvé", status=404)
+        return HttpResponse(f"Nom du groupe mis à jour : {name}")
+
+class UserController(Controller):
+    def __init__(self):
+        super().__init__()
+
+    def get(self, request):
+        get_uuid = request.GET.get('uuid')  # Utilisation UUID.
+        if get_uuid:
+            user = User.objects.get(uuid=get_uuid)
+        else:
+            user = User.objects.all()
+        return super().serialize(user, "user")
+
     def post(self, request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        if password != None :
+        if password:
             hashed_password = make_password(password)
-            new_user = User(username=username,email=email,password=hashed_password)
+            new_user = User(username=username, email=email, password=hashed_password)
             new_user.save()
             messages.success(request, "Formulaire soumis avec succès !")
             return super().serialize(new_user, "user")
-        else :
-            if email == None :
-                email = ""
-            if username == None :
-                username = ""
-            test_user = User.objects.filter(username__startswith=username, email__startswith=email)
-            return super().serialize(test_user, "list")
-        
-    
-    def setUser(request):
-        uuid = request.GET.get('id')
+        else:
+            return HttpResponse("Mot de passe requis", status=400)
+
+    def update_user(self, request):
+        user_uuid = request.GET.get('uuid')  #UUID
         username = request.POST.get('username')
         email = request.POST.get('email')
-        user = User.objects.get(uuid=uuid)
-        user.username = username
-        user.email = email
-        user.save()
-        return HttpResponse(f"Nom d'utilisateur : {username}, Email : {email}")
-    
-    def login(request):
+        try:
+            user = User.objects.get(uuid=user_uuid)
+            user.username = username
+            user.email = email
+            user.save()
+        except User.DoesNotExist:
+            return HttpResponse("Utilisateur non trouvé", status=404)
+        return HttpResponse(f"Utilisateur mis à jour : {username}, {email}")
+
+    def login(self, request):
         email = request.POST.get('email')
         password = request.POST.get('password')
-        user = User.objects.get(email=email)
-        if check_password(password, user.password) :
-            request.session['User'] = str(user.uuid)
-            return HttpResponse(f"Nom d'utilisateur : {user.username}, Email : {email}")
-        else :
-            return HttpResponse("None")
-        
-    def setAdmin(request):
-        getid = request.GET.get('id')
-        test_user = User.objects.get(id=getid)
-        before = test_user.admin
-        if test_user.admin == True :
-            test_user.admin = False
-        else :
-            test_user.admin = True
-        test_user.save()
-        return HttpResponse(f"Avant : {before}, Maintenant : {test_user.admin}")
-    
-    def delete(request):
-        getid = request.GET.get('id')
-        test_user = User.objects.get(id=getid)
-        test_user.delete()
-        return HttpResponse(f"User deleted")
-    
-    
+        try:
+            user = User.objects.get(email=email)
+            if check_password(password, user.password):
+                request.session['user_uuid'] = str(user.uuid)  # Enregistrement de l'UUID dans la session.
+                return HttpResponse(f"Connexion réussie : {user.username}, {email}")
+            else:
+                return HttpResponse("Mot de passe incorrect", status=401)
+        except User.DoesNotExist:
+            return HttpResponse("Utilisateur non trouvé", status=404)
+
+    def toggle_admin(self, request):
+        user_uuid = request.GET.get('uuid')  # Utilisation des UUID pour l'administration.
+        try:
+            user = User.objects.get(uuid=user_uuid)
+            user.admin = not user.admin
+            user.save()
+        except User.DoesNotExist:
+            return HttpResponse("Utilisateur non trouvé", status=404)
+        return HttpResponse(f"Statut admin mis à jour : {user.admin}")
+
+    def delete(self, request):
+        user_uuid = request.GET.get('uuid')  # Suppression par UUID.
+        try:
+            user = User.objects.get(uuid=user_uuid)
+            user.delete()
+        except User.DoesNotExist:
+            return HttpResponse("Utilisateur non trouvé", status=404)
+        return HttpResponse("Utilisateur supprimé")
